c5ea3a9d971844f093ff1a6af6be43d1d7aaf4ca
Devendra Kumar Narware <devn2405@gmail.com>
2026-06-23 21:02:49 +0530

    Add spacing to VStack and improve layout in ContentView

--- test/test/ContentView.swift
+++ test/test/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
        
-        VStack (alignment: .leading){
+        VStack (alignment: .leading, spacing: 10){
             Image("img")
                 .resizable()
                 .scaledToFit()
@@ -42,6 +42,15 @@ struct ContentView: View {
             
             Text("Must place to vist at least once in life")
                 .padding(.leading, 8)
+            
+            HStack{
+                Spacer()
+                Image(systemName: "fork.knife")
+                Image(systemName: "binoculars.fill")
+            }
+            .foregroundStyle(.gray)
+            .font(.caption)
+            .padding(.trailing, 8)
         }
         .padding()
     }
