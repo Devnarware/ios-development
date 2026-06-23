9ebcc467edfb60dfc3608d892af334995b514651
Devendra Kumar Narware <devn2405@gmail.com>
2026-06-23 18:33:41 +0530

    Add detailed layout and images to ContentView for enhanced UI

--- test/test/ContentView.swift
+++ test/test/ContentView.swift
@@ -9,11 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Yoooo!")
+       
+        VStack (alignment: .leading){
+            Image("img")
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(30)
+            
+            HStack{
+                Text("Indus Valley")
+                    .font(.title)
+                    .bold()
+                    
+                Spacer()
+                
+                    Image(systemName: "star.fill")
+                    Image(systemName: "star.fill")
+                    Image(systemName: "star.fill")
+                    Image(systemName: "star.fill")
+                    Image(systemName: "star.leadinghalf.filled")
+                
+            }
+            .padding(.leading, 8)
+            .padding(.trailing, 8)
+            
+            
+            Text("Must place to vist at least once in life")
+                .padding(.leading, 8)
         }
         .padding()
     }
