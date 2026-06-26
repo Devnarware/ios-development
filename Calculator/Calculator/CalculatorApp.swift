046d339ad326d5340cbdca8e8bf95369eb9aa736
Devendra Kumar Narware <devn2405@gmail.com>
2026-06-26 18:01:30 +0530

    start building the calculator

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Devendra kumar narware on 26/06/26.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator/Calculator/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Devendra kumar narware on 26/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        ZStack{
+            Color(.black)
+                .ignoresSafeArea()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
