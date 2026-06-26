88e3d5d44097730e44feec01994998d3a24659d2
Devendra Kumar Narware <devn2405@gmail.com>
2026-06-26 20:00:06 +0530

    building the logic of the calculator

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
+        
+        var arr: [[String]] = [
+            
+            ["7", "8", "9", "+"],
+            ["4", "5", "6", "-"],
+            ["1", "2", "3", "/"]
+        ]
         
         ZStack{
             Color(.black)
                 .ignoresSafeArea()
+            
+            HStack{
+                
+                Spacer()
+                
+                Text("0")
+                    .foregroundStyle(.white)
+                    .font(.largeTitle)
+                    .padding()
+            }
+            // this is the output area
+            
+            VStack{
+                
+                for row in arr{
+                    
+                    HStack{
+                        for col in row{
+                            
+                        }
+                    }
+                }
+            }
         }
     }
 }
